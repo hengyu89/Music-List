a52ca4806b4c6e4f2a6b81e90746d9c7f765aa4a
Heng Yu <heng.yu161@gmail.com>
2023-04-05 19:25:44 -0700

    try to save it

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const path = require('path');
 // Example: http://localhost:3000/images/cat.jpg
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/index.html')));
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
