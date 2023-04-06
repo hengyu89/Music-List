013e1c19636cdbcefca463f7dfd716e88f8fdbe9
Heng Yu <heng.yu161@gmail.com>
2023-04-05 19:10:08 -0700

    Finish import the music list, and do some layout.

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const path = require('path');
+
+// Serve images, css files, js files from the public directory
+// Allows us to reference files with their relative path
+// Example: http://localhost:3000/images/cat.jpg
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
